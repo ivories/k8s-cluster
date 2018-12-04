d522715296259c0c42c16f1e6d2db48b39c22209
Eric.Liu <lyclyc88@users.noreply.github.com>
2018-12-04 18:11:07 +0800

    Update entrypoint.sh

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d /mysql/data ]; then
+if [ -d /mysql/data/mysql ]; then
   echo "[i] MySQL directory already present, skipping creation"
 else
   echo "[i] MySQL data directory not found, creating initial DBs"
